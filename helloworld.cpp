58028463033c8ac757e941e2bb43eae2974f552f
clara <sdfugonclara@gmail.com>
2026-02-28 17:38:14 +0100

    ajout boucle for

--- helloworld.cpp
+++ helloworld.cpp
@@ -7,10 +7,18 @@ using namespace std;
 int main()
 {
    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+   vector <size_t> clara { 1, 2, 3 }; 
 
    for (const string& word : msg)
    {
-      cout << word << " ";
-   }
+        cout << word << " ";
+      for (size_t& number : clara )
+      {
+         std::cout<< number ;
+      
+      }
+    
+   
+}
    cout << endl;
 }
